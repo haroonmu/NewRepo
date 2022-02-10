c6182905d4633c666ba167a9ab8fcc6b540ef36c
haroonmu <haroonmu@deakin.edu.au>
2022-02-10 19:16:40 +0500

    error fixed

--- services/projects.js
+++ services/projects.js
@@ -1,37 +1,48 @@
 const dbo= require("../db/conn");
 
 let projectCollection;
- projectCollection = dbo.getDb().collection("projects");
+//  getProjectCollection() = dbo.getDb().collection("projects");
 // setTimeout(() => {
 // }, 2000);
 
-const getAllProjects = (res) => {
-    
-    projectCollection.find().toArray((err, result) => {
+const getProjectCollection = () =>{
+        if(projectCollection){
+            return projectCollection ;
+        }        
+        try{
+            projectCollection = dbo.getDb().collection("projects");
+            return projectCollection;
+        }catch(e){
+            return null;
+        }
+}
+
+const getAllProjects = (res) => { 
+    getProjectCollection().find().toArray((err, result) => {
         if (err) throw err;
         res.send(result);
     });
 }
 
 const getProjectByID = (id, res) => {
-    //const projectCollection = dbo.getDb().collection("projects");
+    //const getProjectCollection() = dbo.getDb().collection("projects");
     console.log(id);
-    projectCollection.find({ projectID: id }).toArray((err, result) => {
+    getProjectCollection().find({ projectID: id }).toArray((err, result) => {
         if (err) throw err;
         res.send(result);
     });
 }
 
 const insertProject = (project, res) => {
-    //const projectCollection = dbo.getDb().collection("projects");
-    projectCollection.insertOne(project, (err, result) => {
+    //const getProjectCollection() = dbo.getDb().collection("projects");
+    getProjectCollection().insertOne(project, (err, result) => {
         if (err) throw err;
         res.send({ result: 204 });
     });
 }
 
-module.exports=(
+module.exports={
     getAllProjects,
     getProjectByID,
-    insertProject
-)
+  insertProject
+}
